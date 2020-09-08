5806fabec5b58ede0b37ca9b902a2a06d1c2b983
atbeesley <alexander.beesley@gmail.com>
2020-09-08 16:16:45 +0100

    update shopping.actions

--- src/app/store/actions/shopping.actions.ts
+++ src/app/store/actions/shopping.actions.ts
@@ -1,6 +1,19 @@
+import { ShoppingActionTypes } from './shopping.actions';
+import { ShoppingItem } from './../models /shopping-item.model';
 import { Action } from '@ngrx/store'
 import { ShoppingItem } from '../models/shopping-item.model';
 
 export enum ShoppingActionTypes {
     ADD_ITEM = '[SHOPPING] Add Item'
+    ADD_ITEM_SUCCESS = '[SHOPPING] Add Item Success'
+    ADD_ITEM_FAILURE = '[SHOPPING] Add Item Failure'
 }
+
+export class AddItemAction implements Action {
+    readonly: string = ShoppingActionTypes.ADD_ITEM;
+
+    constructor(public pyaload: ShoppingItem){}
+    }
+}
+
+export type ShoppingAction = AddItemAction;
