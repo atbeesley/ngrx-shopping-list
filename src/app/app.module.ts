e22c1a7f6db0f015e4d66482e931af8f9498fe3a
atbeesley <alexander.beesley@gmail.com>
2020-09-10 22:56:25 +0100

    adds imports to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { ShoppingReducer } from './store/reducers/shopping.reducer';
 
 @NgModule({
   declarations: [
@@ -10,8 +11,11 @@ import { StoreModule } from '@ngrx/store';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({}, {})
-  ],
+    FormsModule,
+    StoreModule.forRoot({
+      shopping: ShoppingReducer
+    })
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
